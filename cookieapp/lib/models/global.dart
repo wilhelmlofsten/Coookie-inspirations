1a63cf223d226ad7620c8f0b529e5f82a6fa3593
wilhelm <willo509@student.liu.se>
2022-06-19 15:34:42 +0200

    Added 3 diffrient screens with navbar buttons, todo: content on first page

---
+++ cookieapp/lib/models/global.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+
+Color cookieBrown = new Color(0xFF8b6c5c);
+Color cookieLMediumBrown = new Color(0xFFa08679);
+Color cookieLightBrown = new Color(0xFFd3b6a0);
