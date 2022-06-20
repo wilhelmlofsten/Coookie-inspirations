ec83b98afaced5b9e42a86e80cddba7a16e653ae
wilhelm <willo509@student.liu.se>
2022-06-20 14:49:29 +0200

    Fixed working carousel on third page, uses a list and can/will at a later date add functional controller button

--- cookieapp/lib/main.dart
+++ cookieapp/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'models/global.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,7 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override// Ta bort?
+  @override // Ta bort?
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Cookie-inspirator',
@@ -40,7 +41,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       color: Colors.white,
       home: DefaultTabController(
@@ -58,25 +58,58 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     new Image.network(
-                     ("https://c.tenor.com/3FFKnlwPMf8AAAAC/answer-me-phone.gif"),
-                      //fit: BoxFit.cover,
-                      //scale: 0.1,
-                      width: 250,
-                      height: 250
-                      ),
-                      new Text("King of cookies",
+                        ("https://c.tenor.com/3FFKnlwPMf8AAAAC/answer-me-phone.gif"),
+                        //fit: BoxFit.cover,
+                        //scale: 0.1,
+                        width: 250,
+                        height: 250),
+                    new Text(
+                      "King of cookies",
                       style: intrayPicTitleFont,
-                      ),
+                    ),
                   ],
-                   
                 ),
-               
-        ),
+              ),
               new Container(
                 color: cookieLMediumBrown,
-                ),
+              ),
               new Container(
                 color: cookieBrown,
+                //alignment: Alignment.center,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    new CarouselSlider(
+                      options: CarouselOptions(
+                        autoPlay: true,
+                        enlargeCenterPage: true,
+                        enableInfiniteScroll: true,
+                      ),
+                      items: imgList
+                          .map((e) => ClipRRect(
+                                borderRadius: BorderRadius.circular(8),
+                                child: Stack(
+                                  fit: StackFit.expand,
+                                  children: <Widget>[
+                                    Image.network(
+                                      e,
+                                      width: 1050,
+                                      height: 350,
+                                      fit: BoxFit.cover,
+                                    )
+                                  ],
+                                ),
+                              ))
+                          .toList(),
+                    ),
+                    new Text(
+                      "Pictures of fine Cookies",
+                      style: intrayPicTitleFont,
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
@@ -84,24 +117,23 @@ class _MyHomePageState extends State<MyHomePage> {
             tabs: [
               Tab(
                 icon: new Icon(
-                  Icons.home, 
+                  Icons.home,
                   size: 35,
-                  ),
+                ),
               ),
               Tab(
                 icon: new Icon(
                   Icons.list_alt_sharp,
                   size: 35,
-                  ),
+                ),
               ),
               Tab(
                 icon: new Icon(
                   Icons.image,
                   size: 35,
-                  ),
+                ),
               ),
             ],
-     
             indicatorSize: TabBarIndicatorSize.label,
             labelColor: Colors.blue,
             unselectedLabelColor: Colors.black,
@@ -114,3 +146,14 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+final List<String> imgList = [
+  "https://static.fanpage.it/wp-content/uploads/sites/22/2022/02/Chocolate-chip-cookies-14.jpg",
+  "https://www.seriouseats.com/thmb/tADe-qNstY1RjHrK9t6dj071B_o=/1500x1125/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2017__08__20170727-bravetart-oreos-vicky-wasik-27-2795d8bfdf44495cb9c5e151fc5ef466.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/e/e5/2019-11-29_14_52_43_The_interior_of_a_tin_of_McKenzie_%26_Lloyds_Danish_Style_Butter_Cookies_in_the_Dulles_section_of_Sterling%2C_Loudoun_County%2C_Virginia.jpg",
+  "https://www.sprinklesandsprouts.com/wp-content/uploads/2016/10/Subway-Style-Double-Chocolate-Chip-CookiesANECDOTE.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Ballerina.png/250px-Ballerina.png",
+  "https://celebratingsweets.com/wp-content/uploads/2018/12/MM-Cookies-1-500x500.jpg",
+  "https://fourquarters.bar/wp-content/uploads/2020/12/0_vwtmE6kZFO0rIq9o_.jpg"
+
+];
--- cookieapp/lib/models/global.dart
+++ cookieapp/lib/models/global.dart
@@ -5,14 +5,16 @@ Color cookieBrown = new Color(0xFF8b6c5c); //Custom added colors
 Color cookieLMediumBrown = new Color(0xFFa08679);
 Color cookieLightBrown = new Color(0xFFd3b6a0);
 Color lightGrey = new Color(0xFFEBECF0);
-Color cookieDarkBrown = new Color(0xFF2a0e07); 
+Color cookieDarkBrown = new Color(0xFF2a0e07); //mabye remove
+Color tileBrown = new Color(0xFF4E3524); 
+
 
 //Image cookieMonsterImg = new  Image.network("https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2017%2F11%2Fthe-joy-of-cookies-cookie-monster-blog0817.jpg&q=60");
                    //  https://c.tenor.com/3FFKnlwPMf8AAAAC/answer-me-phone.gif
 
 TextStyle intrayPicTitleFont = new TextStyle(
-  fontFamily: 'Nunito', 
+  fontFamily: 'Raleway', 
   fontWeight: FontWeight.bold, 
-  color: cookieDarkBrown, 
+  color: tileBrown, 
   fontSize: 25
 );
